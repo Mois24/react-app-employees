bba794e3574bbd4f8a2204e1b27ad120709a52ba
Mois24 <artyommois024@gmail.com>
2023-10-04 13:13:31 +0200

    feat: add search panel and add filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,9 +14,11 @@ class App extends Component {
         this.state = {
             data: [
                 {name: 'Nikola Belotti', salary: 1500, increase: false, rise: false, id: 1},
-                {name: 'Roberto Berardi', salary: 1440, increase: false, rise: false, id: 2},
+                {name: 'Roberto Berardi', salary: 1440, increase: false, rise: true, id: 2},
                 {name: 'Alex Drogba', salary: 1769, increase: false, rise: false, id: 3}
-            ]
+            ],
+            term: '', 
+            filter: 'all'
         }
         this.maxId = 4
     }
@@ -49,7 +51,7 @@ class App extends Component {
             return {
                 data: newArr
             }
-        });
+        });        
     }
 
     onToggleIncrease = (id) => {
@@ -87,9 +89,42 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term: term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThan1500':
+               return items.filter(item => item.salary > 1500);
+            default:
+                return items;
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
+
     render() {  
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
-        const increased = this.state.data.filter(item => item.increase).length; 
+        const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+        
         return (
             <div className="app">
                 <AppInfo 
@@ -97,12 +132,15 @@ class App extends Component {
                 increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect}/>
                 </div>
     
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleIncrease={this.onToggleIncrease}
                     onToggleRise={this.onToggleRise}/>
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -19,6 +19,7 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
+        if (this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name: '',
@@ -28,7 +29,6 @@ class EmployeesAddForm extends Component {
 
     render() {
         const {name, salary} = this.state;
-        
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
@@ -48,7 +48,7 @@ class EmployeesAddForm extends Component {
                         value={salary}
                         onChange={this.onValueChange}/>
                     <button type="submit"
-                            className="btn btn-outline-light" >Добавить</button>
+                            className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
         )
